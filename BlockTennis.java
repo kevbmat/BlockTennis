e98941c72349c64a0622f6431bd0a029ba228e64
JohnMaxwellDistinti <JohnMaxwellDistinti@gmail.com>
2019-04-16 09:23:24 -0700

    Paddles w/o flicker

--- BlockTennis.java
+++ BlockTennis.java
@@ -1,64 +1,110 @@
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import javax.swing.Timer;
 import java.util.ArrayList;
 
-public class BlockTennis extends JFrame implements KeyListener, ActionListener{
-	private int paddleWidth = 20; // X dimension for paddles
-	private int paddleHeight = 110; // Y dimension for paddles
-	private int paddleSpeed = 5;
+public class BlockTennis{
+	public static void main(String[] args) {
+        JFrame frame = new JFrame( "Block Tennis" );
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        TennisPanel tp = new TennisPanel();
+
+        //tp.setLayout(new BorderLayout());
+        frame.add(tp);
+        frame.setSize(1800, 750);
+        tp.addKeyListener(tp);
+        ArrayList<Integer> activeKeys = new ArrayList<Integer>();
+        tp.setFocusable(true);
+        tp.requestFocusInWindow();
+        frame.setVisible(true);
+    }
+}
+
+class TennisPanel extends JPanel implements KeyListener, ActionListener{
+	/* WINDOW VARS */
 	private int width = 1800;    // X dimension for window
 	private int height = 750;   // Y dimension for window
+	
+	/* PADDLE VARS */
+	private int paddleWidth = 20; // X dimension for paddles
+	private int paddleHeight = 110; // Y dimension for paddles
+	private int paddleSpeed = 3;
 	private int paddleOnePos = 0; // X dimension for paddles
 	private int paddleTwoPos = 0; // X dimension for paddles
-	private Timer timer;
-	private int delay = 15;
+	
+	/* BALL VARS */
+	private int x = 0;		// x position
+	private int y = 0;		// y position
+	private int radius = 15;	// ball radius
+
+	private int dx = 2;		// increment amount (x coord)
+	private int dy = 2;		// increment amount (y coord)
+	
+	/* TIMER VARS */
+	protected Timer timer;
+	private int delay = 5;
 	ArrayList<Integer> activeKeys = new ArrayList<Integer>();
     //private StatPanel stats;
     //private GamePanel court;
-    public BlockTennis() {
-        // displaying the title
-        setTitle("Block Tennis");
-        // adding a border layout for different panels
-        setLayout(new BorderLayout());
-        timer = new Timer(delay, this);
-        timer.start();
-        addKeyListener(this);
-        pack();
-        setSize(width, height);
-        setVisible(true);
-    }
-    public void paint(Graphics g)
+	public TennisPanel(){
+		timer = new Timer(delay, this);
+		timer.start();		// start the timer
+	}
+	public void getPaddleCollision(int paddleNum, String bound){
+		
+	}
+	public void actionPerformed(ActionEvent arg) {
+		// Search through arraylist for active keys and move paddles
+		for(int i = 0; i < activeKeys.size(); i++) {
+			if (activeKeys.get(i) == KeyEvent.VK_P) {
+				if (paddleTwoPos > (-height/2) + (paddleHeight/2)) {
+					paddleTwoPos = paddleTwoPos - paddleSpeed;
+				}
+			}else if (activeKeys.get(i) == KeyEvent.VK_L) {
+				if (paddleTwoPos < (height/2) - (paddleHeight - 5)) {
+					paddleTwoPos = paddleTwoPos + paddleSpeed;
+				}
+			}else if (activeKeys.get(i) == KeyEvent.VK_Q) {
+				if (paddleOnePos > (-height/2) + (paddleHeight/2)) {
+					paddleOnePos = paddleOnePos - paddleSpeed;
+				}
+			}else if (activeKeys.get(i) == KeyEvent.VK_A) {
+				if (paddleOnePos < (height/2) - (paddleHeight - 5)) {
+					paddleOnePos = paddleOnePos + paddleSpeed;
+				}
+			}
+		}
+		repaint();
+	}
+	public void paintComponent( Graphics g )
     {
-       super.paint(g);
-       //g.drawRect(paddleSizeX, paddleSizeY, (int) (width*.40), height/2);
-       g.drawRect((int) (width*.30), ((int) (height/2))-(paddleHeight/2)+paddleOnePos, paddleWidth, paddleHeight);
-       g.drawRect((int) (width*.70)-paddleWidth, ((int) (height/2))-(paddleHeight/2)+paddleTwoPos, paddleWidth, paddleHeight);
+		super.paintComponent( g );
+		//g.drawRect(paddleSizeX, paddleSizeY, (int) (width*.40), height/2);
+		g.drawRect((int) (width*.30), ((int) (height/2))-(paddleHeight/2)+paddleOnePos, paddleWidth, paddleHeight);
+		g.drawRect((int) (width*.70)-paddleWidth, ((int) (height/2))-(paddleHeight/2)+paddleTwoPos, paddleWidth, paddleHeight);
+		g.setColor(Color.blue);
+		
+		if (x < radius)
+			dx = Math.abs(dx);
+		if (x > getWidth() - radius)	
+			dx = -Math.abs(dx);
+		if (y < radius)			
+			dy = Math.abs(dy);
+		if (y > getHeight() - radius)
+			dy = -Math.abs(dy);
+
+		// adjust ball position
+		x += dx;
+		y += dy;
+		g.fillOval(x - radius, y - radius, radius*2, radius*2);
     }
-    public static void main(String[] args) {
-        new BlockTennis();
-    }
-	@Override
 	public void keyPressed(KeyEvent e) {
 		boolean foundKey = false;
-		/*if (e.getKeyCode() == KeyEvent.VK_P){
-			paddleTwoPos = paddleTwoPos - paddleSpeed;
-			repaint();
-		}else if (e.getKeyCode() == KeyEvent.VK_L) {
-			paddleTwoPos = paddleTwoPos + paddleSpeed;
-			repaint();
-		}
-		if (e.getKeyCode() == KeyEvent.VK_Q) {
-			paddleOnePos = paddleOnePos - paddleSpeed;
-			repaint();
-		}else if (e.getKeyCode() == KeyEvent.VK_A) {
-			paddleOnePos = paddleOnePos + paddleSpeed;
-			repaint();
-		}*/
 		for(int i = 0; i < activeKeys.size(); i++) {
 			if(activeKeys.get(i) == e.getKeyCode()) {
 				foundKey = true;
@@ -68,7 +114,6 @@ public class BlockTennis extends JFrame implements KeyListener, ActionListener{
 			activeKeys.add(e.getKeyCode());
 		}
 	}
-	@Override
 	public void keyReleased(KeyEvent e) {
 		for(int i = 0; i < activeKeys.size(); i++) {
 			if(activeKeys.get(i) == e.getKeyCode()) {
@@ -76,24 +121,7 @@ public class BlockTennis extends JFrame implements KeyListener, ActionListener{
 			}
 		}
 	}
-	@Override
 	public void keyTyped(KeyEvent e) {
 		//System.out.print(e.getKeyCode());
-	}
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// Search through arraylist for active keys and move paddles
-		for(int i = 0; i < activeKeys.size(); i++) {
-			if (activeKeys.get(i) == KeyEvent.VK_P) {
-				paddleTwoPos = paddleTwoPos - paddleSpeed;
-			}else if (activeKeys.get(i) == KeyEvent.VK_L) {
-				paddleTwoPos = paddleTwoPos + paddleSpeed;
-			}else if (activeKeys.get(i) == KeyEvent.VK_Q) {
-				paddleOnePos = paddleOnePos - paddleSpeed;
-			}else if (activeKeys.get(i) == KeyEvent.VK_A) {
-				paddleOnePos = paddleOnePos + paddleSpeed;
-			}
-		}
-		repaint();
 	}
 }
